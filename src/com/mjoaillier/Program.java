6a96df9423fd5e5a579e6fbfc1b05c33d5235c59
MJoaillier <marion.joaillier@outlook.fr>
2018-03-19 16:10:01 +0100

    Rajout d'une exception.

--- src/com/mjoaillier/Program.java
+++ src/com/mjoaillier/Program.java
@@ -1,10 +1,18 @@
 package com.mjoaillier;
 
+import java.io.File;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			File.createTempFile("", null);
+		} catch (Exception erreur) {
+			erreur.printStackTrace();
+		
+		}
+		
 	}
 
 }
